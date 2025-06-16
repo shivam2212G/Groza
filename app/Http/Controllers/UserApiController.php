54559459309163de47620b6a08df773f17bf98cb
Shivam <bulletspeed2005@gmail.com>
2025-06-16 11:43:15 +0530

    All get apis

---
+++ app/Http/Controllers/UserApiController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Subcategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserApiController extends Controller
+{
+    // GET all categories
+    public function getAllCategories()
+    {
+        return response()->json(Category::all());
+    }
+
+    // GET all subcategories of a specific category
+    public function getSubcategoriesByCategory($categoryId)
+    {
+        $subcategories = Subcategory::where('category_id', $categoryId)->get();
+        return response()->json($subcategories);
+    }
+
+    // GET all subcategories
+    public function getAllSubcategories()
+    {
+        return response()->json(Subcategory::all());
+    }
+
+    // GET all products of a specific category
+    public function getProductsByCategory($categoryId)
+    {
+        $products = Product::where('category_id', $categoryId)->get();
+        return response()->json($products);
+    }
+
+    public function getProductsByCategoryAndSubcategory($category_id, $subcategory_id)
+    {
+    $products = Product::where('category_id', $category_id)
+        ->where('subcategory_id', $subcategory_id)
+        ->get();
+
+    return response()->json($products);
+    }
+
+    public function getAllProducts()
+    {
+        $products = DB::table('products')
+        ->join('categories', 'products.category_id', '=', 'categories.category_id')
+        ->join('subcategories', 'products.subcategory_id', '=', 'subcategories.subcategory_id')
+        ->select(
+            'products.product_id',
+            'products.product_name',
+            'products.product_image',
+            'products.product_description',
+            'categories.category_name',
+            'subcategories.subcategory_name'
+        )
+        ->get();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'All Products List',
+            'data' => $products
+        ]);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SubcategoryController;
+use App\Http\Controllers\UserApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//All  Cats
+Route::get('user/categories', [UserApiController::class, 'getAllCategories']);
+// http://127.0.0.1:8000/api/user/categories
+
+//All SubCats
+Route::get('user/subcategories', [UserApiController::class, 'getAllSubcategories']);
+// http://127.0.0.1:8000/api/user/subcategories
+
+//All Pro
+Route::get('user/products', [UserApiController::class, 'getAllProducts']);
+// http://127.0.0.1:8000/api/user/products
+
+//All SubCat by Cat
+Route::get('user/category/{id}/subcategories', [UserApiController::class, 'getSubcategoriesByCategory']);
+// http://127.0.0.1:8000/api/user/category/1/subcategories
+
+//Product by Cat
+Route::get('user/category/{id}/products', [UserApiController::class, 'getProductsByCategory']);
+// http://127.0.0.1:8000/api/user/category/1/products
+
+//Product by Cat & SubCat
+Route::get('user/products/{category_id}/{subcategory_id}', [UserApiController::class, 'getProductsByCategoryAndSubcategory']);
+// http://127.0.0.1:8000/api/user/products/1/2
 
